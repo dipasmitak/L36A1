e8276599dbdba20de904dcab6842c28d7191921d
dipasmitak <dipasmita.kashyap@gmail.com>
2025-10-07 16:48:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+age = 16
+print("My age is:", age, "and its type is:", type(age))
+
+height = 5.2
+print("My height is:", height, "and its type is:", type(height))
+
+name = "Dipasmita"
+print("My name is:", name, "and its type is:", type(name))
+
+is_sunny = True
+print("Is it sunny today?", is_sunny, "and its type is:",type(is_sunny))
+
+fruits = ["dragon fruit", "kiwi", "orange"]
+print("Fruits I like", fruits, "and its type is:", type(fruits))
